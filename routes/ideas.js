c4588f5c80513781aad0882620683d705d55b09f
ross-brown <rbrown_98@yahoo.com>
2023-03-23 00:17:41 -0500

    install nodemon and create routes directory

---
+++ routes/ideas.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const ideas = [
+  {
+    id: 1,
+    text: "Positive NewsLetter, a newsletter that only sharts positive, uplifting news",
+    tag: "Technology",
+    username: "TonyStark",
+    date: "2022-01-02",
+  },
+  {
+    id: 2,
+    text: "Milk cartons that turn a different color the older that your milk is getting",
+    tag: "Inventions",
+    username: "SteveRodgers",
+    date: "2022-01-02",
+  },
+  {
+    id: 3,
+    text: "ATM location app which lets you know where the closest ATM is and if it is in service",
+    tag: "Software",
+    username: "BruceBanner",
+    date: "2022-01-02",
+  },
+];
+
+// Get all ideas
+router.get("/", (req, res) => {
+  res.json({success: true, data: ideas});
+});
+
+// Get a specific idea
+router.get("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+
+  if (!idea) {
+    return res.status(404).json({success: false, error: "Resource not found"});
+  }
+
+  res.json({success: true, data: idea});
+});
+
+module.exports = router;
