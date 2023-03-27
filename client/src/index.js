8b94622a42d5d47cdc3075fdec8e532358b36936
ross-brown <rbrown_98@yahoo.com>
2023-03-27 13:22:54 -0500

    prod build and congif for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
 import IdeaFrom from "./components/IdeaForm";
 import IdeaList from "./components/IdeaList";
